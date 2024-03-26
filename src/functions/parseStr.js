e79ae7e8168b885bc89a175daaf6bfe282ef422b
Dolinin2021 <programmistdolinin@mail.ru>
2024-03-27 00:09:13 +0600

    rebuild

--- src/functions/parseStr.js
+++ src/functions/parseStr.js
@@ -1,7 +1,6 @@
 export default function parseStr(address) {
-    let res = '';
-    let arr;
-    arr = address.split('/');
-    res = arr[0] + '//' + arr[1] + arr[2] + '/backend' + '/' + arr[3] + '/' + arr[4] + '/' + arr[5] + '/' + arr[6];
-    return res;
+    let str = '';
+    let arr = address.split('/');
+    str = arr[0] + '//' + arr[1] + arr[2] + '/backend' + '/' + arr[3] + '/' + arr[4] + '/' + arr[5] + '/' + arr[6];
+    return str;
 }
