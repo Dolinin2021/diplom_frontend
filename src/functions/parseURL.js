bb609bdd67e8ee70e0f58496eb30d39bfbac2cb7
Dolinin2021 <programmistdolinin@mail.ru>
2024-03-27 17:15:34 +0600

    rebuild

--- src/functions/parseURL.js
+++ src/functions/parseURL.js
@@ -2,6 +2,6 @@ export default function parseURL(address) {
     let str = '';
     let data = String(address);
     let arr = data.split('undefined');
-    str = arr[0] + 'backend/' + arr[1];
+    str = arr[0] + arr[1];
     return str;
 }
